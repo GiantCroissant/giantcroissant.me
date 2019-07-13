10940ce28f9c39e00d1dbe4a3ff48109fcdd34a7
ApprenticeGC <apprentice@giantcroissant.com>
2019-07-13 21:52:38 +0800

    Add cms to admin folder as config.yml file
    Update to latest starter project content

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,5 +74,11 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        enableIdentityWidget: true
+      }
+    },
   ],
 }
